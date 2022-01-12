1a5458672262f12bec977919f9a4d964b45c0d6b
AdrielPresley <adriel_tel11@hotmail.com>
2022-01-12 14:22:39 -0300

    Update

--- ExampleBuilders/Program.cs
+++ ExampleBuilders/Program.cs
@@ -7,8 +7,17 @@ namespace ExampleBuilders
 	{
 		static void Main(string[] args)
 		{
-			Pessoa p1 = new Pessoa("Paulo", "Santos");
-			p1.Apresentar();
+			Log log = Log.GetInstance();
+
+			log.PropriedadeLog = "Teste instancia";
+
+			Log log2 = Log.GetInstance();
+			System.Console.WriteLine(log2.PropriedadeLog);
+
+
+
+			//Pessoa p1 = new Pessoa("Paulo", "Santos");
+			//p1.Apresentar();
 		}
 	}
 }
