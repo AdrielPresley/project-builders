4058eedf14c93cf408cd934a57f5d29bc90716fe
AdrielPresley <adriel_tel11@hotmail.com>
2022-01-12 13:03:59 -0300

    Update

--- ExampleBuilders/Models/Pessoa.cs
+++ ExampleBuilders/Models/Pessoa.cs
@@ -3,6 +3,23 @@ namespace ExampleBuilders.Models
     public class Pessoa
     {
         private string nome;
-        private string sobrenome;   
+        private string sobrenome;
+
+        public Pessoa()
+        {
+            nome = string.Empty;
+            sobrenome = string.Empty;
+        }
+
+        public Pessoa(string nome, string sobrenome)
+        {
+            this.nome = nome;
+            this.sobrenome = sobrenome;
+        }
+
+        public void Apresentar()
+        {
+            System.Console.WriteLine($"Olá, meu nome é: {nome} {sobrenome}");
+        }
     }
 }
--- ExampleBuilders/Program.cs
+++ ExampleBuilders/Program.cs
@@ -1,3 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using ExampleBuilders.Models;
+
+namespace ExampleBuilders
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Pessoa p1 = new Pessoa("Paulo", "Santos");
+			p1.Apresentar();
+		}
+	}
+}
 
